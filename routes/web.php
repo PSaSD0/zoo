b058574aed2725344bcd90644c6c3308dc51086e
PSaSD0 <maksponom07@mail.ru>
2026-05-11 22:44:31 +0400

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/home', action: [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
