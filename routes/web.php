5f7d1c4727696fd9dd3f798653edab6b3701524a
PSaSD0 <maksponom07@mail.ru>
2026-05-14 21:50:15 +0400

    полностью сделан каталог

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::get('/catalog', [App\Http\Controllers\WebController::class, 'catalog'])->
 Route::get('/card/{id}', [App\Http\Controllers\WebController::class, 'card'])->name('card');
 Route::get('/articles', [App\Http\Controllers\WebController::class, 'articles'])->name('articles');
 Route::get('/article/{id}', [App\Http\Controllers\WebController::class, 'article'])->name('article');
+Route::get('/search', [App\Http\Controllers\SearchController::class, 'search'])->name('search');
 Route::get('/admin', [App\Http\Controllers\AdminController::class, 'admin'])->name('admin');
 
 Route::post('/addProduct', [App\Http\Controllers\AdminController::class, 'addProduct'])->name('addProduct');
